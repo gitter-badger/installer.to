e28915149ae62db7ab6dd1d7e90594f8ac93165b
Heshdude <heshan.geasman@gmail.com>
2020-05-14 21:14:43 +0530

    Update the git spec to check git version check output

--- installers/git/spec/git_installer_to_spec.sh
+++ installers/git/spec/git_installer_to_spec.sh
@@ -1,9 +1,8 @@
 Describe "Installer script for"
   Describe "git"
-
     It "should install with curl"
       When call bash -c "curl https://installer.to/git | bash"
-      The output should eq "200"
+      The output should include "git version"
     End
   End
 End
